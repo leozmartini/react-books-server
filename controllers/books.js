beec38965b3b4960663531e42f5b34204d117231
demar-nbk <demartinileo@hotmail.com>
2024-03-19 01:11:23 -0300

    Validacoes finalizadas

--- controllers/books.js
+++ controllers/books.js
@@ -1,11 +1,11 @@
 const { getAllBooks, getBookById, insertBook, editBook, deleteBookById } = require("../services/books");
 const { isIDPresent } = require("../services/validations");
 
+const booksDataBase = getAllBooks()
 
 function getBooks(req, res) {
     try {
-        const books = getAllBooks()
-        res.send(books)
+        res.send(booksDataBase)
     } catch (error) {
         res.status(500);
         res.send(error.message)
@@ -15,9 +15,8 @@ function getBooks(req, res) {
 function getBook(req, res) {
     try {
         const id = req.params.id
-        const books = getAllBooks()
-
-        if (isIDPresent(id, books)) {
+        
+        if (isIDPresent(id, booksDataBase)) {
             const book = getBookById(id)
             res.send(book)
         } else {
@@ -32,6 +31,10 @@ function getBook(req, res) {
 
 function postBook(req, res) {
     try {
+        if (!req.body.id) { res.status(422); res.send("Campo id é obrigatório.") }
+        if (isIDPresent(req.body.id, booksDataBase)) { res.status(409); res.send("Este ID já está presente no banco de dados."); return}
+        if (!req.body.nome) { res.status(422); res.send("Campo nome é obrigatório."); return }
+
         insertBook(req.body)
         res.status(201)
         res.send("Novo livro registrado com sucesso")
@@ -43,9 +46,8 @@ function postBook(req, res) {
 function patchBook(req, res) {
     try {
         const id = req.params.id
-        const books = getAllBooks()
-
-        if (isIDPresent(id, books)) {
+    
+        if (isIDPresent(id, booksDataBase)) {
             editBook(req.body, id)
             res.send("Editado com sucesso.")
         } else {
@@ -61,9 +63,8 @@ function patchBook(req, res) {
 function deleteBook(req, res) {
     try {
         const id = req.params.id
-        const books = getAllBooks()
-
-        if (isIDPresent(id, books)) {
+        
+        if (isIDPresent(id, booksDataBase)) {
             deleteBookById(req.params.id)
 
             res.send("Deletado com sucesso.")
